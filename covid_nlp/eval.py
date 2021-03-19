24ef34c3de4329a93b4fb493cf7ca8212f5528b2
hanikanaan <58312140+hanikanaan@users.noreply.github.com>
2021-03-18 21:53:13 -0400

    Question aggregate created
    Modified the eval.py to include the question aggregate. This aggregate was made so that there is a single class object which can reference all required parts needed to take a question, translate it to match the dictionary, answer the question, and return it to the user in the language matching their input language.

--- covid_nlp/eval.py
+++ covid_nlp/eval.py
@@ -1,8 +1,33 @@
 import pandas as pd
 import numpy as np
+import covid_nlp.language.detect_language as detect, covid_nlp.language.ms_translate as translate
+import covid_nlp.eval
+import covid_nlp.modeling.tfidf, covid_nlp.modeling.transformer
 
 from sklearn.metrics import roc_auc_score, f1_score
 from farm.utils import MLFlowLogger
+
+
+class Question():
+
+    def __init__(self):
+        pass
+
+    def search(self, search_string): 
+        input = input()
+        lang = detect.detect_lang_sil(input, input)                  
+        #language detector is called so that the input language is detected
+        search_string = translate.MSTranslator(self, None, None, lang).translate(self, input) 
+        path = 'COVID-QA\data\question-answering'
+        with open(path, 'COVID-QA.json') as f:
+            resu = f.search(search_string)
+        if (detect.LanguageDetector.detect_lang_sil(resu, resu) != lang):
+            resu = translate.MSTranslator(self, None, None, detect.LanguageDetector.detect_lang_sil(resu, resu).translate(self, resu))
+        return resu
+
+
+# This aggregate is made to compile all the different functions that are needed to ask for a question for input, get the input, detect the language in which the input is, translate
+# to the data set language, matching the question to one of the keys in the dictionary in the COVID-QA json file, and then returning and translating (if needed).
 
 
 def eval_question_similarity(y_true, y_pred, lang, model_name, params, user=None, log_to_mlflow=True, run_name="default"):
